8e4b90bbe1f2c39ad0d2f8abc21ff57170e43113
Frost <59969165+phinhas@users.noreply.github.com>
2021-10-11 09:46:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-function myname(){
-    let x=document.getElementById("name").innerHTML="My slack username is frost";
-    return x;
-}
+const pul = document.getElementById('name');
+let newW = 'MY NAME IS PHINEHAS.';
+let index = 1;
+
+const x = setInterval(()=> {
+    pul.innerHTML = newW.slice(0, index);
+    index++;
+
+    if(index > newW.length){
+        index = 1;
+    }
+},200)
